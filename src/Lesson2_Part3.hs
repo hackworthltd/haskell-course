f944743be3f3fff2abdb31e63af4aab5febf8439
Drew Hess <src@drewhess.com>
2020-06-17 19:15:37 +0100

    Lesson2_Part3: trim out some exercises.
    I'll add new ones for the "using functions" section of Lesson2_Part3
    in an upcoming commit.

--- src/Lesson2_Part3.hs
+++ src/Lesson2_Part3.hs
@@ -36,40 +36,38 @@ data Month
   | December
   deriving (Show)
 
-{- Exercise 2.19
+{- Exercise 2.21
 
-Let's revisit the function you wrote in Exercise 2.6, "has31Days".
-When you originally wrote this function, you probably didn't know
-about wildcard patterns yet, so you had to write out all 12 cases by
-hand.
+Let's once again revisit the function you first wrote in Exercise 2.6,
+and then rewrote in Exercise 2.18, "has31Days".
 
-Rewrite this function using wildcard patterns. Use the wildcard
-patterns to reduce it to the smallest number of equations you can.
--}
+Rewrite this function one last time, this time using wildcard patterns
+(_). Use the wildcard patterns to reduce it to the smallest number of
+equations you can. -}
 
 has31Days :: Month -> Bool
 has31Days _ = undefined
 
-{- Exercise 2.20
+{- Exercise 2.22
 
-Define a function named "and" that takes two inputs, both of type
-Bool, and outputs a value of type Bool. The output value should be
-True only if both inputs are True; otherwise, it should be false.
+Compare the version of "has31Days" that you wrote in Exericse 2.21 to
+the version you wrote in Exercise 2.18. Does the latest version have
+fewer equations than the previous one? If not, would you say that the
+newer version is an improvement, and if so, how?
 
-Provide the type signature for this function, as well.
 -}
 
-{- Exercise 2.21
+{- Exercise 2.23
 
 Define a function named "or" that takes two inputs, both of type Bool,
 and outputs a value of type Bool. The output value should be True only
-if one or both of the input values are true; otherwise, it should be
-false.
+if one or both of the input values are True; otherwise, it should be
+False. Write the function using the fewest number of equations you
+can.
 
-Provide the type signature for this function, as well.
--}
+Provide the type signature for this function, as well. -}
 
-{- Exercise 2.22
+{- Exercise 2.24
 
 In Exercise 2.4, you defined a function called "monthToSeason" that,
 given a value of type Month as input, outputs a value of type Season
@@ -88,76 +86,30 @@ input values, one of type Month and the other of type Hemisphere
 (which you just defined), and outputs a value of type Season.
 
 Here we provide you with the incomplete definition for function
-"globalMonthToSeason", but you'll need to provide the rest.
--}
+"globalMonthToSeason", but you'll need to provide the rest. -}
 
 globalMonthToSeason _ _ = undefined
 
-{- Exercise 2.23
-
-For this question, you don't need to write any Haskell code; just
-answer in English here in the comment.
-
-What is the type of the function you just defined above,
-"globalMonthToSeason"?
-
--}
-
-{- Exercise 2.24
-
-Consider the function that you defined in the Exercise 2.22,
-"globalMonthToSeason". You probably had to write quite a few equations
-to define it! Is there a way to reduce the number of equations
-required using pattern wildcards? If so, write the shorter definition;
-if not, explain why pattern wildcards can't help you here.
-
--}
-
-{- NOTE: ****
-
-For these and all future exercises, if we ask you to define a
-function, you should provide the type signature for that function, as
-well. This is a good habit to get into. -}
-
-{- So far, all of the functions you've defined in these exercises have
-taken an input type that has relatively few values. For example, the
-function "follows" from Exercise 2.1 takes an input of type Season,
-which contains only 4 unique values (Winter, Spring, Summer, and
-Fall). Therefore, when defining these functions, we've been able to
-write out all of the equations explicitly for each input value.
-
-Now let's work with some functions that take inputs of type Integer. Type
-Integer contains many, many unique values, so when defining such a
-function, it's usually not possible to write out an explicit equation
-for every case. We'll need to make use of variables in most cases.
-
-To refresh your memory, recall that in Lesson2, part 2, we defined a
-function named "increment" that takes an inputs of type Integer and
-outputs a value of type Integer whose value is produced by adding one
-to the input value. That function's type signature and definition
-were:
-
-increment :: Integer -> Integer
-increment x = x + 1
-
-And when defining it, we used Haskell's built-in function named "+" to
-add the first input to the second. The + function's type signature is:
-
-(+) :: Integer -> Integer -> Integer
-
--}
-
 {- Exercise 2.25
 
-Define a function named "subtract" that takes two inputs of type Integer,
-and outputs a value of type Integer whose value is produced by subtracting
-the value of the second input from the value of the first.
+Consider the function that you defined in the Exercise 2.24,
+"globalMonthToSeason". You probably had to write quite a few equations
+to define it! Is there a way to reduce the number of equations
+required using wildcard patterns (_)? If so, write the shorter
+definition; if not, explain why wildcard patterns can't help you here.
 
-Use the built-in Haskell function "-" to implement the function. Note
-that the "-" function has the type signature:
-
-(-) :: Integer -> Integer -> Integer
 -}
+
+
+
+{- ****
+
+Now stop here and continue on with Lesson2 part 3, starting where you
+left off at the section named "Using functions."
+
+**** -}
+
+
 
 {- Exercise 2.26
 
